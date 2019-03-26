f8d757533a87ce645acfff2c4f2c1b309df81673
seyi-adeleke <seyiadeleke42@yahoo.com>
2019-03-26 22:32:04 +0100

    add test for req 2

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -6,7 +6,11 @@ const expect = require('chai').expect
 describe('Module', () => {
   describe('"isBoolean"', () => {
     it('should export a function', () => {
-      expect(type.isBoolean).to.be.a('function')
+      expect(type.isBoolean).to.be.a('function');
+    });
+
+    it('should return true if a given value is a boolean', () => {
+        expect(type.isBoolean(true)).to.be.true;
     })
   })
 })
